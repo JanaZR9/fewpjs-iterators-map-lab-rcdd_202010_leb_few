a66de1f540374140ba49bf89f598a89b8de9cf86
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-28 16:46:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
-tutorials.map(tutorial => tutorial.charAt(0).toUperCase())
+tutorials.map(tutorial => tutorial.charAt(0).toUpperCase())
